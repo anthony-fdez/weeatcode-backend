718c5aab4300dee28ae81db0ab588349180ca197
Anthony <anthonyfdez23@gmail.com>
2022-06-21 17:06:09 -0400

    Fixed build script to move the code to a separate folder

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import usersRouter from "./api/users/users";
-import { logger } from "../config/logger"
-import { query } from "./db/db";
-
+import { logger } from "../config/logger";
 
 const app = express();
 const PORT = 3001;
@@ -12,16 +10,18 @@ app.use(usersRouter);
 
 const server = app.listen(PORT, () => {
   logger.log({
-    level: 'info',
-    message: `listening on port: ${PORT}`
-  })
-  process.on('unhandledRejection', (err: Error) => {
+    level: "info",
+    message: `listening on port: ${PORT}`,
+  });
+
+  process.on("unhandledRejection", (err: Error) => {
     logger.log({
-      level: 'error',
-      message: `server shutting down due to unhandled rejection: ${err.stack}`
-    }) //just an example on how to use as an error
+      level: "error",
+      message: `server shutting down due to unhandled rejection: ${err.stack}`,
+    }); //just an example on how to use as an error
+
     server.close(() => {
       process.exit(1);
-    })
-  })
+    });
+  });
 });
