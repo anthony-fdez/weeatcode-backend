5a2e5562de076fd577dbcfd03eb2af1bbc5fb4da
Anthony <anthonyfdez23@gmail.com>
2022-06-23 16:06:13 -0400

    Added comments

--- src/middleware/Auth.ts
+++ src/middleware/Auth.ts
@@ -20,6 +20,16 @@ export const Auth = (req: IUserRequest, res: Response, next: NextFunction) => {
   if (!token) return res.status(401).send({ err: "Please log in" });
 
   try {
+    // Here i validate the token, but its just using the email and id to do that
+    // this way lets say the user deletes their account, that they would still have access
+    // if they countinue to use the same token becuase it was never saved in the db
+
+    // I think we should make tokens table, with tokenId, userId and token, and down below
+    // we would just query the db, find a token with their userId, and if its the same we let them
+    // continuew
+
+    // We are pretty much gonna use the jwt as a key, or a hashed password. We wouln't need to verify the token cause we don't care
+
     const { email, userId } = jwt.verify(
       token,
       config.get("JWT_SECRET")
