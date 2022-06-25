5e5239b9795fcbc38eefde1a1e5fcf910723b3fd
Fidel <59423@bears.smcc.edu>
2022-06-25 18:01:03 -0400

    cleaned up using lint script

--- src/api/users/users.ts
+++ src/api/users/users.ts
@@ -1,16 +1,16 @@
-import express, { Router } from "express";
-import signup from "./endpoints/signup";
-import login from "./endpoints/login";
-import deleteUser from "./endpoints/delete";
-import logout from "./endpoints/logout";
-import logoutAll from "./endpoints/logoutAll";
+import express, { Router } from 'express';
+import signup from './endpoints/signup';
+import login from './endpoints/login';
+import deleteUser from './endpoints/delete';
+import logout from './endpoints/logout';
+import logoutAll from './endpoints/logoutAll';
 
 const usersRouter: Router = express.Router();
 
-usersRouter.use("/users", signup);
-usersRouter.use("/users", deleteUser);
-usersRouter.use("/users", login);
-usersRouter.use("/users", logout);
-usersRouter.use("/users", logoutAll);
+usersRouter.use('/users', signup);
+usersRouter.use('/users', deleteUser);
+usersRouter.use('/users', login);
+usersRouter.use('/users', logout);
+usersRouter.use('/users', logoutAll);
 
 export default usersRouter;
