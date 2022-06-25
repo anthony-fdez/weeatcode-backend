5721f219a5590c434674a0c001c41aa99987d4f7
Fidel <59423@bears.smcc.edu>
2022-06-25 17:59:11 -0400

    cleaned up using lint script

--- src/api/users/endpoints/delete.ts
+++ src/api/users/endpoints/delete.ts
@@ -1,25 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import express, { Router, Response } from "express";
-import User from "../../../models/User";
-import Token from "../../../models/Token";
-import { Auth, IUserRequest } from "../../../middleware/Auth";
+import express, { Router, Response } from 'express';
+import User from '../../../models/User';
+import Token from '../../../models/Token';
+import { Auth, IUserRequest } from '../../../middleware/Auth';
 
 const router: Router = express.Router();
 
-const deleteUser = router.post("/delete", Auth, async (req: IUserRequest, res: Response) => {
+const deleteUser = router.post('/delete', Auth, async (req: IUserRequest, res: Response): Promise<void> => {
   /**
    * we first search the user id, if not found json the error
    * if it is found we destroy the token and user
    */
-    try {
-      const deletedUser = await User.destroy({ where: { id: req.user?.userId }});
-      if (!deletedUser) return res.status(400).json({ error: "Could not delete user" });
-      await Token.destroy({ where: { userId: req.user?.userId }});
-      res.json({ msg: "User deleted successfully"});
-    } catch (e) {
-      res.status(500).json({ e });
-    }
+  try {
+    const deletedUser = await User.destroy({ where: { id: req.user?.userId } });
+    if (!deletedUser) res.status(400).json({ error: 'Could not delete user' });
+    await Token.destroy({ where: { userId: req.user?.userId } });
+    res.json({ msg: 'User deleted successfully' });
+  } catch (e) {
+    res.status(500).json({ e });
   }
-);
+});
 
 export default deleteUser;
