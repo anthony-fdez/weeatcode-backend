a9273b06ed365d560ff2500680cdb689900542bf
Anthony <anthonyfdez23@gmail.com>
2022-06-26 00:31:41 -0400

    Added add new post route

--- src/api/posts/endpoints/createPost.ts
+++ src/api/posts/endpoints/createPost.ts
@@ -9,7 +9,35 @@ const createPost = router.post(
   Auth,
   async (req: IUserRequest, res: Response) => {
     try {
-      res.send({ user: req.user });
+      const { title, body } = req.body;
+
+      if (!title || !body) {
+        return res.status(400).send({
+          status: "err",
+          msg: "'title' and 'body' are required",
+        });
+      }
+
+      // Define the post here so its accessible outside the if
+      let post;
+
+      if (req.user?.userId && req.user.userName) {
+        // define the post variable in here so the userid and the name are never undefined
+        post = await Post.create({
+          title,
+          body,
+          authorId: req.user?.userId,
+          authorName: req.user?.userName,
+        });
+      } else {
+        throw new Error("User credentials undefined");
+      }
+
+      if (!post) {
+        throw new Error("Could not create post");
+      }
+
+      res.send({ status: "ok", msg: "Post created", post });
     } catch (err) {
       res.status(500).send({ err, status: "err" });
 
