62a9f1bca9eb8e799f1493bcf8b3d66a39679b31
Anthony <anthonyfdez23@gmail.com>
2022-06-28 12:43:54 -0400

    updated tests

--- src/__tests__/users/signup/users.test.ts
+++ src/__tests__/users/signup/users.test.ts
@@ -2,7 +2,7 @@ import { server } from "./../../../index";
 import supertest from "supertest";
 import sample from "./sample.json";
 // @ts-ignore This package does not support typescript
-// import randomEmail from "random-email";
+import randomEmail from "random-email";
 
 jest.setTimeout(30000);
 const request = supertest(server);
@@ -11,13 +11,13 @@ describe("Register user", () => {
   test("Should create a new user", async () => {
     const res = await request.post("/users/signup").send({
       name: "Test Test",
-      email: "email@email.com",
+      email: randomEmail(),
       password: "12345678",
     });
 
     const { statusCode, body } = res;
-    expect(statusCode).toBe(400);
-    expect(body.status).toBe("err");
+    expect(statusCode).toBe(200);
+    expect(body.status).toBe("ok");
   });
 
   afterAll(async () => {
