3ace7df97a0b0efe38cb1e6141067fdcd18d5d72
Anthony <anthonyfdez23@gmail.com>
2022-06-28 17:13:49 -0400

    Added integration testing

--- src/__tests__/users/integration.test.ts
+++ src/__tests__/users/integration.test.ts
@@ -6,12 +6,13 @@ import randomEmail from "random-email";
 jest.setTimeout(30000);
 const request = supertest(server);
 
-describe("Integration", () => {
+describe("User Integration", () => {
   const email = randomEmail();
   const password = "12345678";
   let userId: number;
   let loginToken: string;
 
+  //Create user
   describe("Signup", () => {
     test("Should create new user", async () => {
       const res = await request.post("/users/signup").send({
@@ -49,6 +50,41 @@ describe("Integration", () => {
     });
   });
 
+  describe("Logout", () => {
+    test("It should log out the user", async () => {
+      const res = await request
+        .post("/users/logout")
+        .set({ Authorization: loginToken });
+
+      const { statusCode, body } = res;
+
+      expect(statusCode).toBe(200);
+      expect(body.status).toBe("ok");
+
+      loginToken = "";
+    });
+  });
+
+  // Need to login again to be able to test the delete endpoint below
+  // since the token was removed when log out
+
+  describe("Login", () => {
+    test("It should log in the user again", async () => {
+      const res = await request.post("/users/login").send({
+        email: email,
+        password: password,
+        createdByTest: true,
+      });
+
+      const { statusCode, body } = res;
+
+      expect(statusCode).toBe(200);
+      expect(body.status).toBe("ok");
+
+      loginToken = body.token;
+    });
+  });
+
   describe("Delete", () => {
     test("It should delete the user", async () => {
       const res = await request
@@ -62,7 +98,7 @@ describe("Integration", () => {
     });
   });
 
-  afterAll(async () => {
-    await server.close();
+  afterAll(() => {
+    server.close();
   });
 });
