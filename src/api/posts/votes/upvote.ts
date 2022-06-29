171d2fcc728c0856a5b2ff4a8a956cf6c1f6ac57
Anthony <anthonyfdez23@gmail.com>
2022-06-29 16:28:08 -0400

    Added downvote route

--- src/api/posts/votes/upvote.ts
+++ src/api/posts/votes/upvote.ts
@@ -3,99 +3,94 @@ import { Auth, IUserRequest } from "../../../middleware/Auth";
 import express, { Router, Response } from "express";
 import PostVote from "../../../models/posts/PostVote";
 import Post from "../../../models/posts/Post";
+import catchAsync from "../../../middleware/catchAsync";
 
 const router: Router = express.Router();
 
 const postUpvote = router.post(
   "/upvote",
   Auth,
-  async (req: IUserRequest, res: Response) => {
-    try {
-      const { postId } = req.body;
+  catchAsync(async (req: IUserRequest, res: Response) => {
+    const { postId } = req.body;
 
-      if (!postId) {
-        return res.status(400).send({
-          status: "err",
-          msg: "Field 'postId' is required",
-        });
-      }
+    if (!postId) {
+      return res.status(400).send({
+        status: "err",
+        msg: "Field 'postId' is required",
+      });
+    }
 
-      const postExists = await Post.findOne({
-        where: {
-          id: postId,
-        },
+    const postExists = await Post.findOne({
+      where: {
+        id: postId,
+      },
+    });
+
+    if (!postExists) {
+      return res.status(400).send({
+        status: "err",
+        msg: `Post with id '${postId}' does not exist`,
+      });
+    }
+
+    const postVoteRecord = (await PostVote.findOne({
+      where: {
+        postId: postId,
+        userId: req.user?.userId,
+        userName: req.user?.userName,
+      },
+    })) as unknown as PostVoteAttributesInterface;
+
+    if (!postVoteRecord) {
+      // If it doesnt exist create one
+
+      if (!req.user?.userId) throw new Error("User id undefined");
+
+      await PostVote.create({
+        userId: req.user?.userId,
+        userName: req.user?.userName,
+        postId,
+        upvote: true,
+        downvote: false,
       });
 
-      if (!postExists) {
-        return res.status(400).send({
-          status: "err",
-          msg: `Post with id '${postId}' does not exist`,
-        });
-      }
+      return res.send({ status: "ok", msg: "Post upvoted" });
+    } else if (postVoteRecord.upvote) {
+      // remove the upvote
 
-      const postVoteRecord = (await PostVote.findOne({
-        where: {
-          postId: postId,
-          userId: req.user?.userId,
-          userName: req.user?.userName,
+      await PostVote.update(
+        {
+          upvote: false,
+          downvote: false,
         },
-      })) as unknown as PostVoteAttributesInterface;
+        {
+          where: {
+            id: postId,
+            userId: req.user?.userId,
+          },
+        }
+      );
 
-      if (!postVoteRecord) {
-        // If it doesnt exist create one
+      return res.send({ status: "ok", msg: "Post upvote removed" });
+    } else {
+      // add the upvote
 
-        if (!req.user?.userId) throw new Error("User id undefined");
-
-        await PostVote.create({
-          userId: req.user?.userId,
-          userName: req.user?.userName,
-          postId,
+      await PostVote.update(
+        {
           upvote: true,
           downvote: false,
-        });
-
-        return res.send({ status: "ok", msg: "Post upvoted" });
-      } else if (postVoteRecord.upvote) {
-        // remove the upvote
-
-        await PostVote.update(
-          {
-            upvote: false,
-            downvote: false,
+        },
+        {
+          where: {
+            id: postId,
+            userId: req.user?.userId,
           },
-          {
-            where: {
-              id: postId,
-              userId: req.user?.userId,
-            },
-          }
-        );
+        }
+      );
 
-        return res.send({ status: "ok", msg: "Post upvote removed" });
-      } else {
-        // add the upvote
-
-        await PostVote.update(
-          {
-            upvote: true,
-            downvote: false,
-          },
-          {
-            where: {
-              id: postId,
-              userId: req.user?.userId,
-            },
-          }
-        );
-
-        return res.send({ status: "ok", msg: "Post upvoted" });
-      }
-    } catch (err) {
-      res.status(500).send({ err, status: "err" });
-
-      console.log(err);
+      return res.send({ status: "ok", msg: "Post upvoted" });
     }
-  }
+  })
 );
 
 export default postUpvote;
