91ed46af9298cd45a4dfc0dc6739597d9e15106d
Anthony <anthonyfdez23@gmail.com>
2022-07-06 23:54:32 -0400

    Added endpoint to get comments under a post

--- src/api/posts/comments/getPostComments.ts
+++ src/api/posts/comments/getPostComments.ts
@@ -1,9 +1,11 @@
+import { CommentVoteAttributesInterface } from "./../../../models/posts/CommentVote";
 import { CommentAttributesInterface } from "./../../../models/posts/Comment";
 import { Auth, IUserRequest } from "../../../middleware/Auth";
 import express, { Router, Response } from "express";
 import Post from "../../../models/posts/Post";
 import catchAsync from "../../../middleware/catchAsync";
 import Comment from "../../../models/posts/Comment";
+import CommentVote from "../../../models/posts/CommentVote";
 
 const router: Router = express.Router();
 
@@ -19,15 +21,45 @@ const getComments = router.post(
         message: "Field 'postId' is required",
       });
 
-    const comments = await Comment.findAll({
+    const comments: CommentAttributesInterface[] = (await Comment.findAll({
       where: {
         postId,
       },
+      include: [
+        {
+          model: CommentVote,
+          as: "commentVotes",
+        },
+      ],
+    })) as unknown as CommentAttributesInterface[];
+
+    const formattedComments: any = [];
+
+    comments.forEach((comment: CommentAttributesInterface, index: number) => {
+      if (!comment.commentVotes) return;
+
+      let upvotes = 0;
+      let downvotes = 0;
+
+      comment.commentVotes.forEach((vote: CommentVoteAttributesInterface) => {
+        if (vote.upvote) {
+          upvotes = upvotes + 1;
+        } else if (vote.downvote) {
+          downvotes = downvotes - 1;
+        }
+      });
+
+      formattedComments.push({
+        upvotes,
+        downvotes,
+        voteScore: upvotes - downvotes,
+        comment,
+      });
     });
 
     res.json({
       status: "ok",
-      comments,
+      comments: formattedComments,
     });
   })
 );
