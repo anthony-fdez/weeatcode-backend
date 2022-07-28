b477422ac98a0f33ced8ebbbeecca9ab4356a8b4
Anthony <anthonyfdez23@gmail.com>
2022-07-27 21:34:15 -0400

    added envoroments scripts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-require("dotenv").config({ path: `.env.development` });
+require("dotenv").config();
 
 import express, { urlencoded } from "express";
 import cors from "cors";
@@ -9,7 +9,6 @@ import usersRouter from "./api/users/users";
 import postsRouter from "./api/posts/posts";
 import error from "./middleware/error";
 
-// Export the app so we can use it it the tests
 const app = express();
 
 app.use(cors());
