037d87da8743f843d31b482a01aa32975b5e7e1b
Anthony <anthonyfdez23@gmail.com>
2022-07-29 17:53:27 -0400

    Added get followers endpoint

--- src/api/users/follow/getFollowers.ts
+++ src/api/users/follow/getFollowers.ts
@@ -1,13 +1,48 @@
+import Follow, {
+  FollowAttributesInterface,
+} from "./../../../models/users/Follow";
+import { AuthOptional } from "./../../../middleware/AuthOptional";
 /* eslint-disable consistent-return */
 import express, { NextFunction, Request, Response, Router } from "express";
 import catchAsync from "../../../middleware/catchAsync";
+import { IUserRequest } from "../../../middleware/Auth";
 
 const router: Router = express.Router();
 
 const getFollowers = router.post(
   "/get_followers",
-  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    res.send("ok");
+  AuthOptional,
+  catchAsync(async (req: IUserRequest, res: Response, next: NextFunction) => {
+    const { userId } = req.body;
+
+    if (!userId)
+      return res.status(400).send({
+        status: "err",
+        message: "Field 'userId' is required.",
+      });
+
+    const followers: FollowAttributesInterface[] = (await Follow.findAll({
+      where: {
+        followingUserId: userId,
+      },
+    })) as unknown as FollowAttributesInterface[];
+
+    const formattedFollowers: any = [];
+
+    await followers.forEach((follower, index) => {
+      let following = false;
+
+      if (req.user?.userId === follower.userId) {
+        following = true;
+      }
+
+      formattedFollowers.push({
+        following,
+        follower,
+      });
+    });
+
+    res.send({ status: "ok", followers: formattedFollowers });
   })
 );
 
